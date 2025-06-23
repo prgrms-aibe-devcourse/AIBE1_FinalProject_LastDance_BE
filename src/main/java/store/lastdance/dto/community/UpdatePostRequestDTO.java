5d5b88e44073f5503e063c8dc1e2e20bc188041b
aibeban <aibeban@github.com>
2025-06-23 11:38:28 +0900

    feat: 커뮤니티 게시글 길이 제한 추가

--- src/main/java/store/lastdance/dto/community/UpdatePostRequestDTO.java
+++ src/main/java/store/lastdance/dto/community/UpdatePostRequestDTO.java
@@ -2,6 +2,7 @@ package store.lastdance.dto.community;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import store.lastdance.domain.community.PostCategory;
@@ -11,9 +12,11 @@ import store.lastdance.domain.community.PostCategory;
 public class UpdatePostRequestDTO {
 
     @NotBlank(message = "제목은 필수입니다.")
+    @Size(max = 100, message = "제목은 100자 이내여야 합니다.")
     private String title;
 
     @NotBlank(message = "내용은 필수입니다.")
+    @Size(max = 2000, message = "내용은 2000자 이내여야 합니다.")
     private String content;
 
     @NotNull(message = "카테고리는 필수입니다.")
