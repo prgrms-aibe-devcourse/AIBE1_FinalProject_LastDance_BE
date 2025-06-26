6360fbb1687ce442b9bc4a63e8760e16c870cd56
soheeGit <63wlsthgml@gmail.com>
2025-06-26 16:23:20 +0900

    feat: 알림 초기세팅

--- src/main/java/store/lastdance/config/RedisConfig.java
+++ src/main/java/store/lastdance/config/RedisConfig.java
@@ -1,28 +1,5 @@
 package store.lastdance.config;
 
-<<<<<<< HEAD
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-@Configuration
-@EnableRedisRepositories
-@RequiredArgsConstructor
-public class RedisConfig {
-
-    private final RedisProperties redisProperties;
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
-=======
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -59,28 +36,10 @@ public class RedisConfig {
                 .build();
 
         return new LettuceConnectionFactory(config, clientConfig);
->>>>>>> dev
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
-<<<<<<< HEAD
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        
-        // Key-Value 직렬화 설정
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        
-        // Hash Key-Value 직렬화 설정  
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        
-        redisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.afterPropertiesSet();
-        
-        return redisTemplate;
-=======
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
 
@@ -94,6 +53,5 @@ public class RedisConfig {
         template.setHashValueSerializer(jackson2JsonRedisSerializer);
 
         return template;
->>>>>>> dev
     }
 }
