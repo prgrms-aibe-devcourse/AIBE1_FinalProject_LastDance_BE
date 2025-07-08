9e877b231d0bdd867fa33040a6e0268f19477a45
hanjungho <thegamerhan@gmail.com>
2025-07-08 17:02:52 +0900

    feat: game DTO 유효성 검증

--- src/main/java/store/lastdance/dto/game/GameResultRequestDTO.java
+++ src/main/java/store/lastdance/dto/game/GameResultRequestDTO.java
@@ -1,13 +1,24 @@
 package store.lastdance.dto.game;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import store.lastdance.domain.game.GameType;
 
 import java.util.List;
 
 public record GameResultRequestDTO(
+
+        @NotNull(message = "게임 타입은 필수 항목입니다.")
         GameType gameType,
-        List<String> participants,
+
+        @NotEmpty(message = "참가자는 최소 1명 이상이어야 합니다.")
+        List<@NotBlank(message = "참가자 이름은 공백일 수 없습니다.") String> participants,
+
+        @NotBlank(message = "게임 결과는 필수 항목입니다.")
         String result,
+
+        @NotBlank(message = "게임 설명은 필수 항목입니다.")
         String penalty
 ) {
 }
