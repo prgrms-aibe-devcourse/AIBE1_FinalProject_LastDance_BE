08ef51527b1b15080b8a24b29664382152876b43
lSNOTNULL <iream1@naver.com>
2025-07-17 05:41:34 +0900

    fix: Redis 접속환경 문제

--- src/main/java/store/lastdance/config/RedisConfig.java
+++ src/main/java/store/lastdance/config/RedisConfig.java
@@ -18,34 +18,10 @@ import java.time.Duration;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
-    @Value("${spring.data.redis.password}")
-    private String password;
-
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(host);
-        config.setPort(port);
-        if (password != null) {
-            config.setPassword(password);
-        }
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .useSsl()
-                .build();
-
-        return new LettuceConnectionFactory(config, clientConfig);
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
 
         // Key는 String으로 직렬화
         template.setKeySerializer(new StringRedisSerializer());
