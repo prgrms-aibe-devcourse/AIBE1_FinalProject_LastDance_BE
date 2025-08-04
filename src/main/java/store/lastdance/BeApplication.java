5bc1545b5205fe95c1d1eb14d7884ac063eb444d
lSNOTNULL <iream1@naver.com>
2025-08-04 10:34:31 +0900

    fix: redis,jpa 명시 , thymeleaf 의존성 제거

--- src/main/java/store/lastdance/BeApplication.java
+++ src/main/java/store/lastdance/BeApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import store.lastdance.config.oauth.OAuth2Properties;
 import java.util.TimeZone;
@@ -12,6 +14,8 @@ import java.util.TimeZone;
 @SpringBootApplication
 @EnableConfigurationProperties(OAuth2Properties.class)
 @EnableScheduling  // 스케줄링 활성화
+@EnableJpaRepositories(basePackages = "store.lastdance.repository")
+@EnableRedisRepositories(basePackages = "store.lastdance.repository.notification")
 //@EnableJpaAuditing  // JPA Auditing 활성화
 public class BeApplication {
 
