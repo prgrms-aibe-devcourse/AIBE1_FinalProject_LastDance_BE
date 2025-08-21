8c9376d20e73360bd4591cdcd55815daa266d394
lSNOTNULL <iream1@naver.com>
2025-08-21 23:06:15 +0900

    refactor: 중복DTO 통합

--- src/main/java/store/lastdance/dto/expense/SaveAnalysisResultRequestDTO.java
+++ src/main/java/store/lastdance/dto/expense/SaveAnalysisResultRequestDTO.java
@@ -2,6 +2,8 @@ package store.lastdance.dto.expense;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import store.lastdance.dto.analysis.AnalyzeExpenseRequestDTO;
+import store.lastdance.dto.analysis.AnalyzeExpenseResponseDTO;
 
 public record SaveAnalysisResultRequestDTO(
         @NotNull @Valid AnalyzeExpenseRequestDTO requestDTO,
